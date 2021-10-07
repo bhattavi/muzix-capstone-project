93933490e04efaf87b3f5e2a91915a32599b4e7e
aren350 <arenrohan@gmail.com>
2021-10-07 21:18:21 +0000

    added backend

---
+++ muzixApp/src/main/java/com/example/muzixApp/MuzixAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.muzixApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzixAppApplication.class, args);
+	}
+
+}
---
+++ muzixApp/src/test/java/com/example/muzixApp/MuzixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muzixApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
