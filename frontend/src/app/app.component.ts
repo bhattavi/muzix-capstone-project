59ca51c7dc5582c55df32d2ee6cb54b51f0ee42d
abhinav bhatt <abhinav.bhatt@pwc.com>
2021-10-11 18:36:06 +0000

    navbar added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'frontend';
+  searchString: String;
+  
+
+  sub: Subscription;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.searchString = '';
+    this.sub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+       
+      }
+    });
+  }
+
+  handleSearch() {
+    this.router.navigate(['/search'], { queryParams: { q: this.searchString } });
+    this.searchString = "";
+  }
+
+  logout() {
+    this.router.navigate(['login']);
+  }
+
+  
 }
